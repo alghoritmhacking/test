05e2897f460dbafd799ca5cf52e2a653a10c057a
test <111470708+testgetcourse@users.noreply.github.com>
2022-09-08 17:35:26 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-ParamsObject_counters_html.firstChild.data = "";
+
 document.getElementById('input_cloud_payments_cp_public_id').value = 'pk_7fb69b31f06d47566ed9397eaf0bb';
 document.getElementById('input_cloud_payments_cp_api_password').value = 'f661fe52053da7a6ce333f899f32a57f';
